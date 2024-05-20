aebc98855f4866bafa1248dbdab1567ac07e8ac8
Negin Sobhani <negin513@gmail.com>
2024-05-20 17:44:08 -0600

    adding data

---
+++ torchrun_multigpu_pbs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#PBS -A SCSG0001
+#PBS -N torchrun_comm
+#PBS -l walltime=01:00:00
+#PBS -l select=2:mpiprocs=1:ncpus=64:ngpus=4:mem=480GB
+#PBS -q main
+#PBS -j oe
+#PBS -k eod
+
+# see set_gpu_rank as a provided module --> sets GPUs to be unique when using MPIs
+# Rory said we might not have to use mpiexec/torchrun once we run that command
+
+# Load modules
+module purge
+module load nvhpc cuda cray-mpich conda 
+conda activate evidential
+
+# Get a list of allocated nodes
+nodes=( $( cat $PBS_NODEFILE ) )
+head_node=${nodes[0]}
+head_node_ip=$(ssh $head_node hostname -i | awk '{print $1}')
+
+echo Head Node IP: $head_node_ip
+echo $(ssh $head_node hostname -i) 2>&1 |tee out
+
+export LSCRATCH=/glade/derecho/scratch/$USER/
+export LOGLEVEL=INFO
+export NCCL_DEBUG=INFO
+export MPICH_GPU_MANAGED_MEMORY_SUPPORT_ENABLED=1
+export MPICH_OFI_NIC_POLICY=GPU
+
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 mpiexec -n 2 --ppn 1 --cpu-bind none torchrun --nnodes=2 --nproc-per-node=4 --rdzv-backend=c10d --rdzv-endpoint=$head_node_ip multigpu_resnet.py 
+
+#mpiexec -n 2 --ppn 4 set_gpu_rank torchrun --nnodes=2 --nproc-per-node=4 --rdzv-backend=c10d --rdzv-endpoint=$head_node_ip test.py

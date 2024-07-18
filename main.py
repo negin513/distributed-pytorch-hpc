04854d729663b0275d87ed73ebd7750ff67c81a9
Negin Sobhani <negin513@gmail.com>
2024-07-17 18:39:41 -0600

    running through black

--- main.py
+++ main.py
@@ -16,30 +16,31 @@ import numpy as np
 import time
 import importlib
 
-if 'LOCAL_RANK' in os.environ:
+if "LOCAL_RANK" in os.environ:
     # Environment variables set by torch.distributed.launch or torchrun
-    LOCAL_RANK = int(os.environ['LOCAL_RANK'])
-    WORLD_SIZE = int(os.environ['WORLD_SIZE'])
-    WORLD_RANK = int(os.environ['RANK'])
-elif 'OMPI_COMM_WORLD_LOCAL_RANK' in os.environ:
+    LOCAL_RANK = int(os.environ["LOCAL_RANK"])
+    WORLD_SIZE = int(os.environ["WORLD_SIZE"])
+    WORLD_RANK = int(os.environ["RANK"])
+elif "OMPI_COMM_WORLD_LOCAL_RANK" in os.environ:
     # Environment variables set by mpirun
-    LOCAL_RANK = int(os.environ['OMPI_COMM_WORLD_LOCAL_RANK'])
-    WORLD_SIZE = int(os.environ['OMPI_COMM_WORLD_SIZE'])
-    WORLD_RANK = int(os.environ['OMPI_COMM_WORLD_RANK'])
+    LOCAL_RANK = int(os.environ["OMPI_COMM_WORLD_LOCAL_RANK"])
+    WORLD_SIZE = int(os.environ["OMPI_COMM_WORLD_SIZE"])
+    WORLD_RANK = int(os.environ["OMPI_COMM_WORLD_RANK"])
 else:
     import sys
+
     sys.exit("Can't find the evironment variables for local rank")
 
-def set_random_seeds(random_seed=0):
 
+def set_random_seeds(random_seed=0):
     torch.manual_seed(random_seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
     np.random.seed(random_seed)
     random.seed(random_seed)
 
-def evaluate(model, device, test_loader):
 
+def evaluate(model, device, test_loader):
     model.eval()
 
     correct = 0
@@ -58,7 +59,6 @@ def evaluate(model, device, test_loader):
 
 
 def main():
-
     num_epochs_default = 10000
     batch_size_default = 256
     image_size_default = 224
@@ -69,21 +69,82 @@ def main():
     steps_syn_default = 20
 
     # Each process runs on 1 GPU device specified by the local_rank argument.
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--local_rank", type=int, help="Local rank. Necessary for using the torch.distributed.launch utility.")
-    parser.add_argument("--num_epochs", type=int, help="Number of training epochs.", default=num_epochs_default)
-    parser.add_argument("--batch_size", type=int, help="Training batch size for one process.", default=batch_size_default)
-    parser.add_argument("--image_size", type=int, help="Size of input image.", default=image_size_default)
-    parser.add_argument("--learning_rate", type=float, help="Learning rate.", default=learning_rate_default)
-    parser.add_argument("--random_seed", type=int, help="Random seed.", default=random_seed_default)
-    parser.add_argument("--model_dir", type=str, help="Directory for saving models.", default=model_dir_default)
-    parser.add_argument("--model_filename", type=str, help="Model filename.", default=model_filename_default)
-    parser.add_argument("--resume", action="store_true", help="Resume training from saved checkpoint.")
-    parser.add_argument("--backend", type=str, help="Backend for distribted training.", default='nccl', choices=['nccl', 'gloo', 'mpi'])
-    parser.add_argument("--arch", type=str, help="Model architecture.", default='resnet50', choices=['resnet50', 'resnet18', 'resnet101', 'resnet152'])
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "--local_rank",
+        type=int,
+        help="Local rank. Necessary for using the torch.distributed.launch utility.",
+    )
+    parser.add_argument(
+        "--num_epochs",
+        type=int,
+        help="Number of training epochs.",
+        default=num_epochs_default,
+    )
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        help="Training batch size for one process.",
+        default=batch_size_default,
+    )
+    parser.add_argument(
+        "--image_size",
+        type=int,
+        help="Size of input image.",
+        default=image_size_default,
+    )
+    parser.add_argument(
+        "--learning_rate",
+        type=float,
+        help="Learning rate.",
+        default=learning_rate_default,
+    )
+    parser.add_argument(
+        "--random_seed", type=int, help="Random seed.", default=random_seed_default
+    )
+    parser.add_argument(
+        "--model_dir",
+        type=str,
+        help="Directory for saving models.",
+        default=model_dir_default,
+    )
+    parser.add_argument(
+        "--model_filename",
+        type=str,
+        help="Model filename.",
+        default=model_filename_default,
+    )
+    parser.add_argument(
+        "--resume", action="store_true", help="Resume training from saved checkpoint."
+    )
+    parser.add_argument(
+        "--backend",
+        type=str,
+        help="Backend for distribted training.",
+        default="nccl",
+        choices=["nccl", "gloo", "mpi"],
+    )
+    parser.add_argument(
+        "--arch",
+        type=str,
+        help="Model architecture.",
+        default="resnet50",
+        choices=["resnet50", "resnet18", "resnet101", "resnet152"],
+    )
     parser.add_argument("--use_syn", action="store_true", help="Use synthetic data")
-    parser.add_argument("--steps_syn", type=int, help="Step per epoch for training with synthetic data", default=steps_syn_default)
-    parser.add_argument("--use_fsdp", action="store_true", help="Use Fully Sharded Data Parallel instead of Distributed Data Parallel")
+    parser.add_argument(
+        "--steps_syn",
+        type=int,
+        help="Step per epoch for training with synthetic data",
+        default=steps_syn_default,
+    )
+    parser.add_argument(
+        "--use_fsdp",
+        action="store_true",
+        help="Use Fully Sharded Data Parallel instead of Distributed Data Parallel",
+    )
 
     argv = parser.parse_args()
 
@@ -104,10 +165,10 @@ def main():
 
     # Create directories outside the PyTorch program
     # Do not create directory here because it is not multiprocess safe
-    '''
+    """
     if not os.path.exists(model_dir):
         os.makedirs(model_dir)
-    '''
+    """
 
     model_filepath = os.path.join(model_dir, model_filename)
 
@@ -115,7 +176,9 @@ def main():
     set_random_seeds(random_seed=random_seed)
 
     # Initializes the distributed backend which will take care of sychronizing nodes/GPUs
-    torch.distributed.init_process_group(backend=backend, rank=WORLD_RANK, world_size=WORLD_SIZE)
+    torch.distributed.init_process_group(
+        backend=backend, rank=WORLD_RANK, world_size=WORLD_SIZE
+    )
 
     # Encapsulate the model on the GPU assigned to the current process
     model = getattr(torchvision.models, argv.arch)(pretrained=False)
@@ -125,8 +188,10 @@ def main():
 
     if argv.use_fsdp:
         ddp_model = FullyShardedDataParallel(model, flatten_parameters=True)
-    else: 
-        ddp_model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[LOCAL_RANK], output_device=LOCAL_RANK)
+    else:
+        ddp_model = torch.nn.parallel.DistributedDataParallel(
+            model, device_ids=[LOCAL_RANK], output_device=LOCAL_RANK
+        )
 
     # We only save the model who uses device "cuda:0"
     # To resume, the device for the saved model would also be "cuda:0"
@@ -140,39 +205,57 @@ def main():
         labels_syn = torch.zeros(batch_size, dtype=torch.int64).to(device)
     else:
         # Prepare dataset and dataloader
-        transform = transforms.Compose([
-            transforms.RandomCrop(32, padding=4),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-        ])
+        transform = transforms.Compose(
+            [
+                transforms.RandomCrop(32, padding=4),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
+                ),
+            ]
+        )
 
         # Data should be prefetched
         # Download should be set to be False, because it is not multiprocess safe
-        train_set = torchvision.datasets.CIFAR10(root="data", train=True, download=True, transform=transform) 
-        test_set = torchvision.datasets.CIFAR10(root="data", train=False, download=True, transform=transform)
+        train_set = torchvision.datasets.CIFAR10(
+            root="data", train=True, download=True, transform=transform
+        )
+        test_set = torchvision.datasets.CIFAR10(
+            root="data", train=False, download=True, transform=transform
+        )
 
         # Restricts data loading to a subset of the dataset exclusive to the current process
         train_sampler = DistributedSampler(dataset=train_set)
 
-        train_loader = DataLoader(dataset=train_set, batch_size=batch_size, sampler=train_sampler, num_workers=8)
+        train_loader = DataLoader(
+            dataset=train_set,
+            batch_size=batch_size,
+            sampler=train_sampler,
+            num_workers=8,
+        )
         # Test loader does not have to follow distributed sampling strategy
-        test_loader = DataLoader(dataset=test_set, batch_size=128, shuffle=False, num_workers=8)
+        test_loader = DataLoader(
+            dataset=test_set, batch_size=128, shuffle=False, num_workers=8
+        )
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(ddp_model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=1e-5)
+    optimizer = optim.SGD(
+        ddp_model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=1e-5
+    )
 
     # Loop over the dataset multiple times
     times = []
     for epoch in range(num_epochs):
-
         print("Local Rank: {}, Epoch: {}, Training ...".format(LOCAL_RANK, epoch))
-        
+
         # Save and evaluate model routinely
         if not use_syn:
             if epoch % 10 == 0:
                 if LOCAL_RANK == 0:
-                    accuracy = evaluate(model=ddp_model, device=device, test_loader=test_loader)
+                    accuracy = evaluate(
+                        model=ddp_model, device=device, test_loader=test_loader
+                    )
                     torch.save(ddp_model.state_dict(), model_filepath)
                     print("-" * 75)
                     print("Epoch: {}, Accuracy: {}".format(epoch, accuracy))
@@ -194,7 +277,11 @@ def main():
 
             if epoch > 0:
                 times.append(elapsed)
-                print('num_steps_per_gpu: {}, avg_step_time: {:.4f}'.format(count, elapsed / count))              
+                print(
+                    "num_steps_per_gpu: {}, avg_step_time: {:.4f}".format(
+                        count, elapsed / count
+                    )
+                )
         else:
             train_loader.sampler.set_epoch(epoch)
             start_epoch = time.time()
@@ -213,12 +300,16 @@ def main():
 
             if epoch > 0:
                 times.append(elapsed)
-                print('num_steps_per_gpu: {}, avg_step_time: {:.4f}'.format(count, elapsed / count))
+                print(
+                    "num_steps_per_gpu: {}, avg_step_time: {:.4f}".format(
+                        count, elapsed / count
+                    )
+                )
 
     avg_time = sum(times) / (num_epochs - 1)
 
     print("Average epoch time: {}".format(avg_time))
 
+
 if __name__ == "__main__":
-    
     main()

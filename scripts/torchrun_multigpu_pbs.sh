77bb84d0ba4e71c4692524dddf49da7b503cb4f6
Negin Sobhani <negin513@gmail.com>
2024-07-26 16:58:58 -0600

    clean up

--- scripts/torchrun_multigpu_pbs.sh
+++ scripts/torchrun_multigpu_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## This script is for running a multi-GPU job on Derecho using torchrun and MPI. 
-
+## This script runs 
 
 #PBS -A SCSG0001         
 #PBS -N torchrun_multigpu
@@ -14,10 +14,14 @@
 # Load modules
 module purge
 module load nvhpc cuda cray-mpich conda
-
+#module load conda intel cray-mpich cuda cudnn
 ## Load the conda environment
-conda activate evidential
+#conda activate evidential
+conda activate /glade/work/schreck/conda-envs/holodec
+#conda activate /glade/work/schreck/conda-envs/credit
+#conda activate torch_cuda
 
+which torchrun
 
 #########################################
 # Determine the number of nodes:
@@ -50,33 +54,65 @@ if (( nnodes > 1)); then
 fi
 
 #########################################
-export LSCRATCH=/glade/derecho/scratch/$USER/
-export LOGLEVEL=INFO
-export NCCL_DEBUG=INFO
+
+export NCCL_SOCKET_IFNAME=hsn # needed for NCCL --> not working without this. 
+
+# no performance improvement with these settings:
 export MPICH_GPU_MANAGED_MEMORY_SUPPORT_ENABLED=1
 export MPICH_OFI_NIC_POLICY=GPU
+export MPICH_GPU_SUPPORT_ENABLED=1
 
-export LOGLEVEL=INFO
-export NCCL_DEBUG=INFO
+# needed to avoid hangs by Josh Romero (NVIDIA):
+#export FI_CXI_DISABLE_HOST_REGISTER=1
+#export FI_CXI_OPTIMIZED_MRS=false
 
+# -- old settings not sure if they had large impacts:
+
+export NCCL_SHM_DISABLE=1 # probably doesn't matter
+export NCCL_IB_DISABLE=1  # probably doesn't matter
+export NCCL_CROSS_NIC=1 # should have an impact based on docs but it does not. (Josh Romero said improve perf on non-rail optimized networks)
+export NCCL_NCHANNELS_PER_NET_PEER=4
+#export NCCL_NET_GDR_LEVEL=PHB
+###exexport OMP_NUM_THREADS=16
+
+
+#export NCCL_HOME=/glade/u/apps/common/23.08/spack/opt/spack/nvhpc/24.1/Linux_x86_64/24.1/comm_libs/12.3/nccl
+#export LD_LIBRARY_PATH=$NCCL_HOME/lib:$NCCL_HOME/plugin/lib:$LD_LIBRARY_PATH
+
+DEBUG=0
+if [ "$ENABLE_NCCL_OFI" -eq 1 ]; then
+    export LSCRATCH=/glade/derecho/scratch/negins/
+    export LOGLEVEL=INFO
+    export NCCL_DEBUG=INFO
+    #export NCCL_DEBUG=TRACE #VERSION
+else    
+    export NCCL_DEBUG=VERSION
+fi
 
 #########################################
-# NCCL OFI settings (from Daniel Howard):
-export NCCL_SOCKET_IFNAME=hsn
-export NCCL_HOME=/glade/u/home/dhoward/work/nccl-ofi-plugin/install
-export LD_LIBRARY_PATH=$NCCL_HOME/lib:$NCCL_HOME/plugin/lib:$LD_LIBRARY_PATH
+ENABLE_NCCL_OFI=0  # Set to 1 to enable, 0 to disable
 
-export NCCL_NCHANNELS_PER_NET_PEER=4
-export MPICH_GPU_SUPPORT_ENABLED=1
-export MPICH_OFI_NIC_POLICY=GPU
-export MPICH_RDMA_ENABLED_CUDA=1
-#export NCCL_DISABLE_IB=1 ## not correct variable in Daniel's email:
-export NCCL_IB_DISABLE=1
-export NCCL_CROSS_NIC=1
-export FI_CXI_DISABLE_HOST_REGISTER=1
-export FI_CXI_OPTIMIZED_MRS=false
-export NCCL_NET="AWS Libfabric"
-export NCCL_NET_GDR_LEVEL=PBH
+activate_nccl_ofi() {
+  local enable_nccl_ofi=$1
+
+  if [ "$enable_nccl_ofi" -eq 1 ]; then
+    #########################################
+    # NCCL OFI settings (from Daniel Howard):
+    export NCCL_HOME=/glade/u/home/dhoward/work/nccl-ofi-plugin/install
+    export LD_LIBRARY_PATH=$NCCL_HOME/lib:$NCCL_HOME/plugin/lib:$LD_LIBRARY_PATH
+
+    export NCCL_NCHANNELS_PER_NET_PEER=4
+    export MPICH_GPU_SUPPORT_ENABLED=1
+    export MPICH_OFI_NIC_POLICY=GPU
+    export MPICH_RDMA_ENABLED_CUDA=1
+    export NCCL_IB_DISABLE=1
+    export NCCL_CROSS_NIC=1
+    export NCCL_NET="AWS Libfabric" # not needed from Negin's opinion
+    export NCCL_NET_GDR_LEVEL=PBH
+    #export NCCL_DEBUG=INFO
+    #########################################
+  fi
+}
 
 #########################################
 
@@ -97,6 +133,32 @@ fi
 CMD="$LAUNCHER main.py"
 echo "Training Command: $CMD"
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 mpiexec -n 2 --ppn 1 --cpu-bind none torchrun --nnodes=2 --nproc-per-node=4 --rdzv-backend=c10d --rdzv-endpoint=$head_node_ip multigpu_resnet.py 
+which mpiexec
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 
-#mpiexec -n 2 --ppn 4 set_gpu_rank torchrun --nnodes=2 --nproc-per-node=4 --rdzv-backend=c10d --rdzv-endpoint=$head_node_ip test.py
+
+LOGFILE=resnet_benchmark.log
+
+ENABLE_NCCL_OFI=0  # Set to 1 to enable, 0 to disable
+activate_nccl_ofi $ENABLE_NCCL_OFI
+
+# -- does not work:
+#python -m torch.utils.collect_env
+echo "------------------------------------" >> $LOGFILE
+echo "ENABLE_NCCL_OFI" $ENABLE_NCCL_OFI >> $LOGFILE
+date +"%Y-%m-%d %H:%M:%S" >> $LOGFILE
+rm -f snapshot.pt
+
+mpiexec -n 2 --ppn 1 --cpu-bind none torchrun --nnodes=2 --nproc-per-node=4 --rdzv-backend=c10d --rdzv-endpoint=$head_node_ip main.py 
+
+
+ENABLE_NCCL_OFI=1
+activate_nccl_ofi $ENABLE_NCCL_OFI
+echo "ENABLE_NCCL_OFI" $ENABLE_NCCL_OFI >> $LOGFILE
+
+rm -f snapshot.pt
+
+mpiexec -n 2 --ppn 1 --cpu-bind none torchrun --nnodes=$nnodes --nproc-per-node=auto --rdzv-backend=c10d --rdzv-endpoint=$head_node_ip main.py 
+date +"%Y-%m-%d %H:%M:%S" >> $LOGFILE
+
+echo "------------------------------------" >> $LOGFILE

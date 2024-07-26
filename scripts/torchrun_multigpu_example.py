0728ac6dd25cb224b36f3b7bbc5e3e91c97c73b3
Negin Sobhani <negin513@gmail.com>
2024-07-26 14:29:53 -0600

    an example

--- scripts/torchrun_multigpu_example.py
+++ scripts/torchrun_multigpu_example.py
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-## This script runs a multi-GPU job on Derecho using torchrun and MPI.
-## This script runs speed tests for different communication backends.
+## This script is for running a multi-GPU job on Derecho using torchrun and MPI. 
+## This script runs 
 
-#PBS -A SCSG0001
-#PBS -N performance_test
+#PBS -A SCSG0001         
+#PBS -N torchrun_multigpu
 #PBS -l walltime=01:00:00
-#PBS -l select=2:mpiprocs=1:ncpus=64:ngpus=4
+#PBS -l select=2:mpiprocs=1:ncpus=64:ngpus=4:mem=480GB
 #PBS -q main
 #PBS -j oe
 ##PBS -k eod
 
-
 # Load modules
 module purge
-#module load nvhpc cuda cudnn cray-mpich 
-module load conda nvhpc cray-mpich
-#module load conda intel cray-mpich
-
-
+#module load nvhpc cuda cray-mpich conda
+module load conda intel cray-mpich
 ## Load the conda environment
+#conda activate evidential
 conda activate /glade/work/schreck/conda-envs/holodec
 #conda activate /glade/work/schreck/conda-envs/credit
 #conda activate torch_cuda
@@ -49,14 +46,16 @@ if (( nnodes > 1)); then
     head_node=${nodes[0]}
     #head_node_ip=$(ssh $head_node hostname -I | awk '{print $1}')
     head_node_ip=$(ssh $head_node hostname -i | awk '{print $1}')
+
     echo "head_node_ip: $head_node_ip"
     
     # write the head node IP to a file 
     echo $(ssh $head_node hostname -i) > out
 fi
 
-
 #########################################
+
+export NCCL_SOCKET_IFNAME=hsn # needed for NCCL --> not working without this. 
 
 export NCCL_SOCKET_IFNAME=hsn # needed for NCCL --> not working without this. 
 
@@ -71,8 +70,8 @@ export FI_CXI_OPTIMIZED_MRS=false
 
 # -- old settings not sure if they had large impacts:
 
-#export NCCL_SHM_DISABLE=1 # probably doesn't matter
-#export NCCL_IB_DISABLE=1  # probably doesn't matter
+export NCCL_SHM_DISABLE=1 # probably doesn't matter
+export NCCL_IB_DISABLE=1  # probably doesn't matter
 export NCCL_CROSS_NIC=1 # should have an impact based on docs but it does not. (Josh Romero said improve perf on non-rail optimized networks)
 #export NCCL_NET_GDR_LEVEL=PHB
 ###exexport OMP_NUM_THREADS=16
@@ -90,7 +89,6 @@ if [ "$ENABLE_NCCL_OFI" -eq 1 ]; then
 else    
     export NCCL_DEBUG=VERSION
 fi
-
 
 #########################################
 ENABLE_NCCL_OFI=0  # Set to 1 to enable, 0 to disable
@@ -117,43 +115,35 @@ activate_nccl_ofi() {
   fi
 }
 
-
 #########################################
 
+##CMD="torchrun --nodes --nnodes=$nnodes --nproc-per-node=auto --rdzv-backend=c10d --rdzv-endpoint=$head_node_ip main.py"
+##echo "Training Command: $CMD"
+
+#-- torchrun launch configuration
+
+LAUNCHER="torchrun "
+LAUNCHER+="--nnodes=$nnodes --nproc_per_node=auto --max_restarts 0 "
+
+if [[ "$nnodes" -eq 1 ]]; then
+    LAUNCHER+="--standalone "
+else
+    LAUNCHER+="--rdzv_backend=c10d --rdzv_endpoint=$head_node_ip"
+fi
+
+CMD="$LAUNCHER main.py"
+echo "Training Command: $CMD"
 
 which mpiexec
 export CUDA_VISIBLE_DEVICES=0,1,2,3
-
 
 ENABLE_NCCL_OFI=0  # Set to 1 to enable, 0 to disable
 activate_nccl_ofi $ENABLE_NCCL_OFI
 
 # -- does not work:
 #python -m torch.utils.collect_env
-echo "------------------------------------" >> benchmark_results.log
-echo "ENABLE_NCCL_OFI" $ENABLE_NCCL_OFI >> benchmark_results.log
 
-# send_recv speed tests (nccl vs. gloo):
-mpiexec -n 2 --ppn 1 --cpu-bind none torchrun --nnodes=2 --nproc-per-node=4 --rdzv-backend=c10d --rdzv-endpoint=$head_node_ip send_recv_test.py --backend nccl
-mpiexec -n 2 --ppn 1 --cpu-bind none torchrun --nnodes=2 --nproc-per-node=4 --rdzv-backend=c10d --rdzv-endpoint=$head_node_ip send_recv_test.py --backend gloo
+rm -f snapshot.pt
 
+mpiexec -n 2 --ppn 1 --cpu-bind none torchrun --nnodes=2 --nproc-per-node=4 --rdzv-backend=c10d --rdzv-endpoint=$head_node_ip main.py 
 
-# allreduce speed tests (nccl vs. gloo):
-mpiexec -n 2 --ppn 1 --cpu-bind none torchrun --nnodes=2 --nproc-per-node=4 --rdzv-backend=c10d --rdzv-endpoint=$head_node_ip all_reduce_test.py --backend nccl
-mpiexec -n 2 --ppn 1 --cpu-bind none torchrun --nnodes=2 --nproc-per-node=4 --rdzv-backend=c10d --rdzv-endpoint=$head_node_ip all_reduce_test.py --backend gloo
-
-ENABLE_NCCL_OFI=1
-activate_nccl_ofi $ENABLE_NCCL_OFI
-
-echo "ENABLE_NCCL_OFI" $ENABLE_NCCL_OFI >> benchmark_results.log
-# send_recv speed tests (nccl vs. gloo):
-mpiexec -n 2 --ppn 1 --cpu-bind none torchrun --nnodes=2 --nproc-per-node=4 --rdzv-backend=c10d --rdzv-endpoint=$head_node_ip send_recv_test.py --backend nccl
-mpiexec -n 2 --ppn 1 --cpu-bind none torchrun --nnodes=2 --nproc-per-node=4 --rdzv-backend=c10d --rdzv-endpoint=$head_node_ip send_recv_test.py --backend gloo
-
-
-# allreduce speed tests (nccl vs. gloo):
-mpiexec -n 2 --ppn 1 --cpu-bind none torchrun --nnodes=2 --nproc-per-node=4 --rdzv-backend=c10d --rdzv-endpoint=$head_node_ip all_reduce_test.py --backend nccl
-mpiexec -n 2 --ppn 1 --cpu-bind none torchrun --nnodes=2 --nproc-per-node=4 --rdzv-backend=c10d --rdzv-endpoint=$head_node_ip all_reduce_test.py --backend gloo
-
-
-echo "------------------------------------" >> benchmark_results.log

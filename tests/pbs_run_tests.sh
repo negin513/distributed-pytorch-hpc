1e8ebc0d564ac36cba45b58a2f7c35d9d964e873
Negin Sobhani <negin513@gmail.com>
2024-08-01 15:02:48 -0600

    update pbs script

--- tests/pbs_run_tests.sh
+++ tests/pbs_run_tests.sh
@@ -19,7 +19,7 @@ module load conda intel cray-mpich
 
 
 ## Load the conda environment
-conda activate pytorch_cuda_env
+conda activate /glade/work/negins/conda-envs/pytorch_cuda_env
 #conda activate /glade/work/schreck/conda-envs/holodec
 
 which torchrun

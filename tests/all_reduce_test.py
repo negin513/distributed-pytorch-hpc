0a326c7f0d9f715269fca058abc101b95f5e1a6b
Negin Sobhani <negin513@gmail.com>
2024-08-22 15:59:28 -0600

    updating all_reduce_test.py

--- tests/all_reduce_test.py
+++ tests/all_reduce_test.py
@@ -5,17 +5,19 @@ import statistics
 import socket
 import torch
 import torch.distributed as dist
-print ("hello!")
-try:
+import socket
+
+try: 
     from mpi4py import MPI
     comm = MPI.COMM_WORLD
     shmem_comm = comm.Split_type(MPI.COMM_TYPE_SHARED)
-
+    
     LOCAL_RANK = shmem_comm.Get_rank()
     WORLD_SIZE = comm.Get_size()
     WORLD_RANK = comm.Get_rank()
-    os.environ['MASTER_ADDR'] = comm.bcast(socket.gethostbyname(socket.gethostname()), root=0)
-    os.environ['MASTER_PORT'] = '29500'
+
+    os.environ['MASTER_ADDR'] = comm.bcast( socket.gethostbyname( socket.gethostname() ), root=0 )
+    os.environ['MASTER_PORT'] =	'1234'
 
 except:
     if "LOCAL_RANK" in os.environ:
@@ -28,8 +30,14 @@ except:
         LOCAL_RANK = int(os.environ["OMPI_COMM_WORLD_LOCAL_RANK"])
         WORLD_SIZE = int(os.environ["OMPI_COMM_WORLD_SIZE"])
         WORLD_RANK = int(os.environ["OMPI_COMM_WORLD_RANK"])
+    elif "PMI_RANK" in os.environ:
+        # Environment variables set by cray-mpich
+        LOCAL_RANK = int(os.environ["PMI_LOCAL_RANK"])
+        WORLD_SIZE = int(os.environ["PMI_SIZE"])
+        WORLD_RANK = int(os.environ["PMI_RANK"])
     else:
-        print ("Error: No environment variables set for distributed training")
+        import sys
+        sys.exit("Can't find the evironment variables for local rank")
 
 print (LOCAL_RANK, WORLD_SIZE, WORLD_RANK) 
 
